0d74f7c07b35c4e8a0d937702486cb59327a9a2d
P0646084 <andre.tancredo@portoseguro.com.br>
2022-11-17 22:06:12 -0300

    usando o hooks  state

--- frontend/src/components/SalesCard/index.tsx
+++ frontend/src/components/SalesCard/index.tsx
@@ -1,3 +1,4 @@
+import { useState } from "react";
 import DatePicker from "react-datepicker";
 import "react-datepicker/dist/react-datepicker.css";
 
@@ -6,6 +7,14 @@ import './styles.css';
 
 
 function SalesCard() {
+//alterando estado das datas, pegando o data minima e o maxima, e setando o data min e a max. Função nativa do js que usa o estado. 
+//Aqui a baixo a variavel min recebe uma nova data setando ela ou pegando uma nova data informada e diminuindo em um ano.
+const min = new Date(new Date().setDate(new Date().getDate() - 365));
+const max = new Date(); 
+
+const [minDate, setMinDate] = useState(min);
+const [maxDate, setMaxDate] = useState(max);
+
   return (
 
     <div className="dsmeta-card">
@@ -13,16 +22,16 @@ function SalesCard() {
       <div>
         <div className="dsmeta-form-control-container">
           <DatePicker
-            selected={new Date()}
-            onChange={(date: Date) => { }}
+            selected={minDate}
+            onChange={(date: Date) => setMinDate(date)}
             className="dsmeta-form-control"
             dateFormat="dd/MM/yyyy"
           />
         </div>
         <div className="dsmeta-form-control-container">
           <DatePicker
-            selected={new Date()}
-            onChange={(date: Date) => { }}
+            selected={maxDate}
+            onChange={(date: Date) => setMaxDate(date)}
             className="dsmeta-form-control"
             dateFormat="dd/MM/yyyy"
           />
