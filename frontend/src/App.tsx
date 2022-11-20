19ebc561029c370422ac56dc76668b532e463278
P0646084 <andre.tancredo@portoseguro.com.br>
2022-11-20 17:00:08 -0300

    Toast

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,11 +1,13 @@
 // import NotificationButton from "./components/NotificationButton"
 import SalesCard from "./components/SalesCard"
 import Header from "./components/header"
+import { ToastContainer } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 
 function App() {
   return (
     <>
-
+      <ToastContainer/>
       <Header />
 
       <main>
