573178af1ab273bfdb36db2d7c2e03060417d2c2
P0646084 <andre.tancredo@portoseguro.com.br>
2022-11-20 09:42:59 -0300

    Consulta por data

--- backend/src/main/java/com/primeirobackend/dsmeta/services/SaleService.java
+++ backend/src/main/java/com/primeirobackend/dsmeta/services/SaleService.java
@@ -1,8 +1,13 @@
 package com.primeirobackend.dsmeta.services;
 
-import java.util.List;
+
+import java.time.Instant;
+import java.time.LocalDate;
+import java.time.ZoneId;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 
 import com.primeirobackend.dsmeta.entities.Sale;
@@ -14,8 +19,18 @@ public class SaleService {
 	@Autowired
 	private SaleRepository repository;
 	
-	public List<Sale> findSales() {
-	return repository.findAll();
+	public Page<Sale> findSales(String minDate, String maxDate, Pageable pageable) { //esse pageable serve para implementar uma paginação com 20 itens de cada vez.
+		
+		LocalDate today = LocalDate.ofInstant(Instant.now(), ZoneId.systemDefault());  //criando uma data no dia de hoje.
+		
+		
+		
+		//esse equals serve pra isso, fazer essa comparação. Se for igual...
+		LocalDate min = minDate.equals("") ? today.minusDays(365) : LocalDate.parse(minDate);
+		LocalDate max = maxDate.equals("") ? today : LocalDate.parse(maxDate);  //aqui é assim: se maxDate for igual ao texto vazio, ele recebe today, se nao segue o padrao do local date.
+		
+		
+	return repository.findSales(min, max, pageable);
 	}
 
 }
